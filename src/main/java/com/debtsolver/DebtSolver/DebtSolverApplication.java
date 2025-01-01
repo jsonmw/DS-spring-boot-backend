057702337a196f25c0e2d99fd7f59559b7438dce
jasonw <jasonmwild@gmail.com>
2025-01-01 15:14:18 -0800

    Sprint initializr project added

---
+++ src/main/java/com/debtsolver/DebtSolver/DebtSolverApplication.java
@@ -0,0 +1,13 @@
+package com.debtsolver.DebtSolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtSolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/debtsolver/DebtSolver/DebtSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debtsolver.DebtSolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
