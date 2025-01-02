28c1dd9db895f555d2ab988c40e5e2600ea7b8ef
jasonw <jasonmwild@gmail.com>
2025-01-01 22:11:19 -0800

    Refactored modelMapping to use generics

--- src/main/java/com/debtsolver/DebtSolver/service/impl/DebtServiceImpl.java
+++ src/main/java/com/debtsolver/DebtSolver/service/impl/DebtServiceImpl.java
@@ -2,6 +2,7 @@ package com.debtsolver.DebtSolver.service.impl;
 
 import com.debtsolver.DebtSolver.dto.DebtDTO;
 import com.debtsolver.DebtSolver.exception.ResourceNotFoundException;
+import com.debtsolver.DebtSolver.mapping.EntityDTOMapper;
 import com.debtsolver.DebtSolver.model.Debt;
 import com.debtsolver.DebtSolver.repository.DebtRepository;
 import com.debtsolver.DebtSolver.service.DebtService;
@@ -25,7 +26,7 @@ public class DebtServiceImpl implements DebtService {
     /**
      * Retrieves specified debt
      *
-     * @param debtId: id for desired debt
+     * @param debtId:  id for desired debt
      * @param ownerId: id for associated owner
      * @return DTO containing fetched debt
      */
@@ -36,7 +37,7 @@ public class DebtServiceImpl implements DebtService {
 
         log.info("Fetching debt {}", debt);
 
-        return mapToDebtDTO(debt);
+        return EntityDTOMapper.mapEntityToDTO(debt, DebtDTO.class);
     }
 
     /**
@@ -50,32 +51,15 @@ public class DebtServiceImpl implements DebtService {
 
         List<Debt> list = debtRepository.findByOwnerId(ownerId);
         log.info("Fetching all debts for user id: {} -> {}", ownerId, list);
+        if (list.isEmpty()) {
+            log.warn("No debts found for user id: {}", ownerId);
+        }
 
-        List<DebtDTO> listOfDebts = list.stream().map(debt -> mapToDebtDTO(debt))
+        List<DebtDTO> listOfDebts = list.stream().map(debt -> EntityDTOMapper.mapEntityToDTO(debt, DebtDTO.class))
                 .collect(Collectors.toList());
 
         return listOfDebts;
     }
 
-    /**
-     * Mapper method to convert expense entity to expense DTO
-     *
-     * @param debt: Debt object to be converted
-     * @return resulting DebtDTO
-     */
-    private DebtDTO mapToDebtDTO(Debt debt) {
-        return modelMapper.map(debt, DebtDTO.class);
-    }
-
-    /**
-     * Mapper method to convert expense DTO to expense entity
-     *
-     * @param debt: DTO object to be converted
-     * @return a Debt object from the DTO data
-     */
-    private Debt mapToDebtEntity(DebtDTO debt) {
-        return modelMapper.map(debt, Debt.class);
-
-    }
 }
 
