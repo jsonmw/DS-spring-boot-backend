868394f26b700d310da280bc40d79adfddaedc19
jasonw <jasonmwild@gmail.com>
2025-01-02 23:03:13 -0800

    Basic controller code finished

--- src/main/java/com/debtsolver/DebtSolver/controller/DebtController.java
+++ src/main/java/com/debtsolver/DebtSolver/controller/DebtController.java
@@ -1,13 +1,35 @@
 package com.debtsolver.DebtSolver.controller;
 
+import com.debtsolver.DebtSolver.dto.DebtDTO;
+import com.debtsolver.DebtSolver.io.DebtRequest;
+import com.debtsolver.DebtSolver.io.DebtResponse;
+import com.debtsolver.DebtSolver.service.DebtService;
+import com.debtsolver.DebtSolver.util.MappingUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class DebtController {
 
-
+    private final DebtService debtService;
+    /**
+     * Creates new debt in database
+     *
+     * @param debtRequest
+     * @return debtResponse
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/user/{ownerId}")
+    public DebtResponse saveExpenseDetails(@RequestBody @Valid DebtRequest debtRequest, @PathVariable Long ownerId) {
+        log.info("API POST /user/{} called {}", ownerId, debtRequest);
+        DebtDTO debtDTO = MappingUtil.mapToNewClass(debtRequest, DebtDTO.class);
+        debtDTO = debtService.createNewDebt(debtDTO);
+        log.info("Printing the debt DTO {}", debtDTO);
+        return MappingUtil.mapToNewClass(debtDTO, DebtResponse.class);
+    }
 }
